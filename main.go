723f9f545b099cc1d6a72ec9be49575d51a29bcb
Johan <johanjern95@gmail.com>
2021-01-03 21:42:59 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,36 +1,9 @@
 package main
 
 import (
-	"database/sql"
-	"log"
-	"oikotie/config"
-	"oikotie/scraper"
-
 	"oikotie/cmd"
 )
 
 func main() {
 	cmd.Execute()
-	return
-	cfg, err := config.NewReader()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db, err := sql.Open("postgres", cfg.DatabaseURL())
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// search := scraper.Create(db).SetAreaCodes([]string{"00200", "00340"})
-	// res, err := search.Run()
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Println(res)
-
-	err = scraper.UpdateListing(db, 4)
-	if err != nil {
-		log.Fatal(err)
-	}
 }
