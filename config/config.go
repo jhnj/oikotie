016e645244fe5c7dfee3a8b20920acdea26687be
Johan <johanjern95@gmail.com>
2021-01-03 21:41:30 +0200

    config file

--- config/config.go
+++ config/config.go
@@ -1,14 +1,29 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
+type SearchConfig struct {
+	Areas []string `json:"areas"`
+	Price struct {
+		Min int `json:"min"`
+		Max int `json:"max"`
+	} `json:"price"`
+	Size struct {
+		Min int `json:"min"`
+		Max int `json:"max"`
+	} `json:"size"`
+}
+
 type Reader struct {
-	cache map[string]string
+	cache        map[string]string
+	searchConfig *SearchConfig
 }
 
 func NewReader() (*Reader, error) {
@@ -17,7 +32,7 @@ func NewReader() (*Reader, error) {
 		return nil, err
 	}
 
-	return &Reader{map[string]string{}}, nil
+	return &Reader{map[string]string{}, nil}, nil
 }
 
 func (r Reader) get(key string) string {
@@ -36,4 +51,20 @@ func (r Reader) get(key string) string {
 
 func (r Reader) DatabaseURL() string {
 	return r.get("DATABASE_URL")
+}
+
+func (r Reader) SearchConfig() *SearchConfig {
+	if r.searchConfig == nil {
+		raw, err := ioutil.ReadFile(r.get("SEARCH_CONFIG_PATH"))
+		if err != nil {
+			panic(fmt.Errorf("Failed to read config"))
+		}
+
+		err = json.Unmarshal(raw, r.searchConfig)
+		if err != nil {
+			panic(fmt.Errorf("Failed to parse config"))
+		}
+	}
+
+	return r.searchConfig
 }
